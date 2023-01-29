0b3549e19c889e3c477e3d42bc0a4b16ac910b5d
root <root@wks64794.emap-intl.net>
2023-01-29 11:33:05 -0300

    fix

--- src/tools/CSV/csv.py
+++ src/tools/CSV/csv.py
@@ -1,4 +1,5 @@
 import csv
+import os
 from entity.Restaurant import Restaurant
 
 def getFileName(rest: Restaurant, add: str):
@@ -12,6 +13,7 @@ def createFile(data: Restaurant):
     nameOption = getFileName(data, 'options')
     headersMenu = getHeaders(data, 'menu')
     headersOption = getHeaders(data, 'option')
+    make_sure_path_exists("files")
     with open(nameMenu, 'w', encoding='UTF8') as f:
             writer = csv.DictWriter(f, fieldnames=headersMenu, delimiter='|',  dialect=csv.excel_tab)
             writer.writeheader()
@@ -27,3 +29,12 @@ def createFile(data: Restaurant):
         "menuPath": nameMenu,
         "optionFilePath": nameOption
     }
+
+
+
+def make_sure_path_exists(path):
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if not os.path.isdir(path):
+            raise
